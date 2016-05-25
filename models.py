2f416e6375a7c2bf727ccd11e5978d129c5375ef
Harrow Coffee Pot <harrowcoffeepot@gmail.com>
2016-05-25 14:52:31 +0000

    25/5/16

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 #import datetime
 from django.db                              import models
-from django.utils                           import timezone
+#from django.utils                           import timezone
 from django.contrib.auth.models             import User
 
 class Person(models.Model):
@@ -10,14 +10,18 @@ class Person(models.Model):
   fullmember              = models.BooleanField()
   status                  = models.IntegerField()
   authorname              = models.CharField(max_length=20,blank = True, null = True)
-  detailcolor             = models.CharField(max_length=7)
-  attendeescolor          = models.CharField(max_length=7)
-  backgroundcolor         = models.CharField(max_length=7)
+  reversevideo            = models.BooleanField(default=False)
+  datecolor               = models.CharField(max_length=20, default='black')
+  detailcolor             = models.CharField(max_length=20, default='#0000C0')
+  attendeescolor          = models.CharField(max_length=20, default='#00C000')
+  backgroundcolor         = models.CharField(max_length=20, default='#F3FFF3')
+  datecolor_rev           = models.CharField(max_length=20, default='white')
+  detailcolor_rev         = models.CharField(max_length=20, default='aqua')
+  attendeescolor_rev      = models.CharField(max_length=20, default='lawngreen')
+  backgroundcolor_rev     = models.CharField(max_length=20, default='black')
   last_login              = models.DateTimeField(blank = True, null = True)
   def __str__(self):
     return str(self.username)
-
-
 
 
 
